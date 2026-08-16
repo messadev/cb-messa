a174457bf4b01f2e65fc2e0c3c146b7af48fce56
messadev <nonepixel8@gmail.com>
2026-08-17 00:40:13 +0500

    fix: file position #1

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
+// an array containing multiple map objects with fake user data
 export const users = [
   { id: 1, name: "Alikhan", age: 18, active: true },
   { id: 2, name: "Aruzhan", age: 18, active: false },
@@ -5,6 +6,7 @@ export const users = [
   { id: 4, name: "Amirkhan", age: 11, active: true },
 ];
 
+// an array containing multiple map objects with fake order data
 export const orders = [
   { id: 1, userId: 1, total: 5000, status: "completed" },
   { id: 2, userId: 3, total: 12000, status: "pending" },
@@ -12,3 +14,12 @@ export const orders = [
   { id: 4, userId: 2, total: 3000, status: "cancelled" },
   { id: 5, userId: 3, total: 9000, status: "completed" },
 ];
+
+// a map object containing fake user data
+export const user = {
+  id: 1,
+  name: "Ali",
+  age: 22,
+  email: "ali@example.com",
+  active: true,
+};
