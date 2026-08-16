f9ab9a75b6ea53d221ff7e1152e0de22fab1f3a9
messadev <nonepixel8@gmail.com>
2026-08-16 21:56:28 +0500

    fix: file management

---
+++ data.js
@@ -0,0 +1,14 @@
+export const users = [
+  { id: 1, name: "Alikhan", age: 18, active: true },
+  { id: 2, name: "Aruzhan", age: 18, active: false },
+  { id: 3, name: "Danial", age: 30, active: true },
+  { id: 4, name: "Amirkhan", age: 11, active: true },
+];
+
+export const orders = [
+  { id: 1, userId: 1, total: 5000, status: "completed" },
+  { id: 2, userId: 3, total: 12000, status: "pending" },
+  { id: 3, userId: 1, total: 7000, status: "completed" },
+  { id: 4, userId: 2, total: 3000, status: "cancelled" },
+  { id: 5, userId: 3, total: 9000, status: "completed" },
+];
--- functions.js
+++ functions.js
@@ -4,12 +4,7 @@ i never got to build something properly in js.
 i know only the theoretical part.
 */
 
-const users = [
-  { id: 1, name: "Alikhan", age: 18, active: true },
-  { id: 2, name: "Aruzhan", age: 18, active: false },
-  { id: 3, name: "Danial", age: 30, active: true },
-  { id: 4, name: "Amirkhan", age: 11, active: true },
-];
+import { users, orders } from "./data.js";
 
 const getActiveUsers = (data) => {
   const filtered = data.filter((user) => user.active && user.age >= 18);
@@ -47,14 +42,6 @@ const getAverageAge = (data) => {
 };
 
 // console.log(getAverageAge(users));
-
-const orders = [
-  { id: 1, userId: 1, total: 5000, status: "completed" },
-  { id: 2, userId: 3, total: 12000, status: "pending" },
-  { id: 3, userId: 1, total: 7000, status: "completed" },
-  { id: 4, userId: 2, total: 3000, status: "cancelled" },
-  { id: 5, userId: 3, total: 9000, status: "completed" },
-];
 
 const getCompletedRevenue = (data) => {
   const completedOrders = data.filter((order) => order.status === "completed");
