d8902e0bde1a22982d05e669ed3949b733d7a8b1
messadev <nonepixel8@gmail.com>
2026-08-17 14:56:07 +0500

    new: data

--- js/data.js
+++ js/data.js
@@ -23,3 +23,9 @@ export const user = {
   email: "ali@example.com",
   active: true,
 };
+
+export const tables = [
+  { id: 1, name: "Table 1", booked: false, bookedBy: null },
+  { id: 2, name: "Table 2", booked: false, bookedBy: null },
+  { id: 3, name: "Table 3", booked: true, bookedBy: "Ali" },
+];
