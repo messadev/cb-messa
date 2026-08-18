e956aec06cb2cde5a0bcbd771e50f4ed00a19dcc
messadev <nonepixel8@gmail.com>
2026-08-19 01:56:57 +0500

    feat: data validation

--- js/case/form.js
+++ js/case/form.js
@@ -1,10 +1,19 @@
-const submitFormBtn = document.querySelector("#booking-form");
+const bookingForm = document.querySelector("#booking-form");
 
-submitFormBtn.addEventListener("submit", (e) => {
+bookingForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const data = new FormData(submitFormBtn);
+  const data = new FormData(bookingForm);
   const formattedData = Object.fromEntries(data);
 
+  formattedData.guests = Number(formattedData.guests);
+
   console.log(formattedData);
+
+  const errorElement = document.querySelector("#form-error");
+
+  if (!formattedData.name.trim()) {
+    errorElement.textContent = "ОШИБКА: Введите имя";
+    return;
+  }
 });
