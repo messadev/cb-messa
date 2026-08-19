bf9ee7f7e39544a8d3ef6fe287991ef609ed4687
messadev <nonepixel8@gmail.com>
2026-08-19 17:01:08 +0500

    fix: guest number validation

--- js/case/form.js
+++ js/case/form.js
@@ -8,12 +8,21 @@ bookingForm.addEventListener("submit", (e) => {
 
   formattedData.guests = Number(formattedData.guests);
 
-  console.log(formattedData);
-
   const errorElement = document.querySelector("#form-error");
 
   if (!formattedData.name.trim()) {
     errorElement.textContent = "ОШИБКА: Введите имя";
     return;
+  } else if (!formattedData.phone.trim()) {
+    errorElement.textContent = "ОШИБКА: Введите номер телефона";
+    return;
+  } else if (0 >= formattedData.guests || formattedData.guests >= 10) {
+    errorElement.textContent =
+      "ОШИБКА: Кол-во гостей не может меньше 0 и больше 10";
+    return;
+  } else {
+    errorElement.textContent = "";
+
+    console.log(formattedData);
   }
 });
