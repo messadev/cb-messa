27b9fb3845db4cdab608e0427e66f6b4e228e255
messadev <nonepixel8@gmail.com>
2026-08-20 23:51:11 +0500

    fix: re-appearing userslist

--- js/case/users.js
+++ js/case/users.js
@@ -2,34 +2,32 @@ const loadUsersBtn = document.querySelector("#load-users");
 const currentStatus = document.querySelector("#status");
 const usersList = document.querySelector("#users");
 
-loadUsersBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  currentStatus.textContent = "Loading...";
+const loadUsersData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-  const loadUsersData = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users",
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP Error: ${response.status}`);
-      }
-
-      const data = await response.json();
-      data.forEach((user) => {
-        const userElement = document.createElement("p");
-
-        userElement.textContent = `${user.name} - ${user.email}`;
-        userElement.style.fontWeight = "bold";
-
-        usersList.append(userElement);
-      });
-    } catch (e) {
-      currentStatus.textContent = e;
-      console.error(e);
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
     }
-  };
 
+    currentStatus.textContent = `Отлично, мы получили все нужные данные!`;
+
+    const data = await response.json();
+    data.forEach((user) => {
+      const userElement = document.createElement("p");
+
+      userElement.textContent = `${user.name} - ${user.email}`;
+      userElement.style.fontWeight = "bold";
+
+      usersList.append(userElement);
+    });
+  } catch (e) {
+    currentStatus.textContent = e;
+    console.error(e);
+  }
+};
+
+loadUsersBtn.addEventListener("click", () => {
+  currentStatus.textContent = "Loading...";
   loadUsersData();
 });
