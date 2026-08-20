2ab031ad768534e65055f12b5c938b83ef006b74
messadev <nonepixel8@gmail.com>
2026-08-20 23:39:44 +0500

    feat: fetch

--- js/case/users.js
+++ js/case/users.js
@@ -1,5 +1,29 @@
-button = document.querySelector("#button");
+const loadUsersBtn = document.querySelector("#load-users");
+const currentStatus = document.querySelector("#status");
+const usersList = document.querySelector("#users");
 
-button.addEventListener("click", () => {
-  console.log("Hello!");
+loadUsersBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  currentStatus.textContent = "Loading...";
+
+  const loadUsersData = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+
+      if (!response.ok) {
+        currentStatus.textContent = `Ошибка при запросе данных: ${response.status}`;
+      } else {
+        currentStatus.textContent = `Отлично, мы получили данные!`;
+      }
+
+      const data = await response.json();
+      // data.map((u) => ); i don't get this part, how do i return those data to html?
+    } catch (e) {
+      console.error(`Ошибка при запросе данных: ${e}`);
+    }
+  };
+
+  loadUsersData();
 });
